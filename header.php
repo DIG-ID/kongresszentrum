a77c41f46169bbcdd37eb1d88d9773fa7b6246d8
Pedro Silva <pedro@dig.id>
2022-03-15 18:17:08 +0000

    up

--- header.php
+++ header.php
@@ -8,6 +8,15 @@
 		<meta name="viewport" content="width=device-width, initial-scale=1.0" >
 		<link rel="profile" href="https://gmpg.org/xfn/11">
 
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-110180671-3"></script>
+		<script>
+			window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+			gtag('config', 'UA-110180671-3');
+		</script>
+
 		<?php wp_head(); ?>
 
 	</head>
